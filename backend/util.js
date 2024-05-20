07bba517abca4a96ff1a0b317aae34b5389d12b4
saleh-expansionjs <saleh@expansionjs.com>
2024-05-20 23:28:54 +0530

    completed whole transaction process

--- backend/util.js
+++ backend/util.js
@@ -1,3 +1,6 @@
+const fetch = require("node-fetch");
+const { encryptEas } = require("./Getepay_pg_node/components/encryptEas");
+const { decryptEas } = require("./Getepay_pg_node/components/decryptEas");
 const { DateTime } = require("luxon");
 const moment = require("moment-timezone");
 
@@ -9,4 +12,95 @@ function generateDate(req) {
   return formattedDate;
 }
 
-module.exports = { generateDate };
+function getepayPortal(data, config) {
+  return new Promise(async (resolve, reject) => {
+    const JsonData = JSON.stringify(data);
+    var ciphertext = encryptEas(
+      JsonData,
+      config["GetepayKey"],
+      config["GetepayIV"]
+    );
+    var newCipher = ciphertext.toUpperCase();
+    var myHeaders = {
+      "Content-Type": "application/json",
+    };
+    var raw = JSON.stringify({
+      mid: data.mid,
+      terminalId: data.terminalId,
+      req: newCipher,
+    });
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+    try {
+      const requestUrl = await fetch(config["GetepayUrl"], requestOptions);
+      const result = await requestUrl.text();
+
+      const resultobj = JSON.parse(result);
+      const responseurl = resultobj.response;
+      const dataitem = decryptEas(
+        responseurl,
+        config["GetepayKey"],
+        config["GetepayIV"]
+      );
+      const parsedData = JSON.parse(dataitem);
+      resolve(parsedData);
+    } catch (err) {
+      reject(err);
+    }
+  });
+}
+
+async function generatePaymentUrl(req) {
+  try {
+    const currentDate = generateDate(req);
+    const data = {
+      mid: "108",
+      amount: "2.00",
+      merchantTransactionId: process.env.MERCHANT_ID,
+      transactionDate: currentDate,
+      terminalId: process.env.TERMINAL_ID,
+      udf1: "7665730788",
+      udf2: "saleh.majeed8@gmail.com",
+      udf3: "saleh",
+      udf4: "",
+      udf5: "",
+      udf6: "",
+      udf7: "",
+      udf8: "",
+      udf9: "",
+      udf10: "",
+      ru: "https://pay1.getepay.in:8443/getepayPortal/pg/pgPaymentResponse",
+      callbackUrl: process.env.CALLBACK_URL,
+      currency: "INR",
+      paymentMode: "ALL",
+      bankId: "",
+      txnType: "single",
+      productType: "IPG",
+      txnNote: "Test Txn",
+      vpa: process.env.VPA,
+    };
+
+    const config = {
+      GetepayMid: 108,
+      GeepayTerminalId: process.env.VPA,
+      GetepayKey: process.env.GET_E_PAY_KEY,
+      GetepayIV: process.env.GET_E_PAY_IV,
+      GetepayUrl:
+        "https://pay1.getepay.in:8443/getepayPortal/pg/generateInvoice",
+    };
+
+    const { paymentId: transactionID, paymentUrl } = await getepayPortal(
+      data,
+      config
+    );
+    return { transactionID, paymentUrl };
+  } catch (err) {
+    return err.message;
+  }
+}
+
+module.exports = { generateDate, generatePaymentUrl };
